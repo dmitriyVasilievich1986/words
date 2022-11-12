a35d5291e80e492c0f417d838dca2301d8fd76d2
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-11-12 12:14:00 +0100

    add main html file

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from rest_framework.response import Response
-from .models import Verb, VerbDeclension, Pronoun
-from random import choice
-from django.http.response import HttpResponse
 
 
 def index_view(request):
-    ch = choice(choice(Verb.objects.all()).verb_declentions.all())
-    pron = ch.pron.word
-    verb = ch.word
-    return HttpResponse(f"{pron} {verb}")
+    return render(request, "index.html")
