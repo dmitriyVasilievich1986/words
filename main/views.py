f42470c0cd880538bcd1ad28b977801e4ab10542
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-11-12 11:34:45 +0100

    add simple view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Verb, VerbDeclension, Pronoun
+from random import choice
+from django.http.response import HttpResponse
 
-# Create your views here.
+
+def index_view(request):
+    ch = choice(choice(Verb.objects.all()).verb_declentions.all())
+    pron = ch.pron.word
+    verb = ch.word
+    return HttpResponse(f"{pron} {verb}")
