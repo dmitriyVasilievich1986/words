01c93b733ec1d4b87e4e9518fd6619cf4fe2865e
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-11-12 19:04:03 +0100

    add api read

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,31 @@
 import React from "react";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { setState } from "../reducers/wordReducer";
 
 function App() {
+  const verb = useSelector((state) => state.words.verb);
+  const dispatch = useDispatch();
+
+  React.useEffect((_) => {
+    Promise.all([
+      axios.get("/api/verbdeclension/"),
+      axios.get("/api/verb/"),
+      axios.get("/api/pron/"),
+    ])
+      .then((values) => {
+        const [verbdeclensionData, verbData, pronData] = values;
+        dispatch(
+          setState({
+            verbDeclension: verbdeclensionData.data,
+            verb: verbData.data,
+            pron: pronData.data,
+          })
+        );
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
   return <div>App</div>;
 }
 
