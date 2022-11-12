0013e1b618c01fda24ddfca04b583da4eb0f6176
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-11-12 11:34:33 +0100

    add models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from random import choices
+from enum import Enum
+
+
+Pronoun = {
+    "ja": "я",
+    "ti": "ти",
+    "on": "он",
+    "vi": "ви",
+    "mi": "ми",
+    "oni": "они",
+}
+
 
 # Create your models here.
+class Pron(models.Model):
+    translate = models.CharField(max_length=150, blank=False, null=False)
+    word = models.CharField(max_length=150, blank=False, null=False)
+
+
+class Verb(models.Model):
+    translate = models.CharField(max_length=150, blank=False, null=False)
+    word = models.CharField(max_length=150, blank=False, null=False)
+
+
+class VerbDeclension(models.Model):
+    translate = models.CharField(max_length=150, blank=False, null=False)
+    word = models.CharField(max_length=150, blank=False, null=False)
+
+    verb = models.ForeignKey(
+        related_name="verb_declentions",
+        on_delete=models.CASCADE,
+        to="Verb",
+    )
+    pron = models.ForeignKey(
+        related_name="verb_declentions",
+        on_delete=models.CASCADE,
+        to="Pron",
+    )
