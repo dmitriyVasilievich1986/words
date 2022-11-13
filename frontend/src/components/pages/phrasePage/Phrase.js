b90058595adc886e96af946274625a149cf69fc6
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-11-13 20:22:09 +0100

    move Phrase to separate comp

---
+++ frontend/src/components/pages/phrasePage/Phrase.js
@@ -0,0 +1,40 @@
+import { getAdjCaseRandom, getPronRandom } from "Reducers/wordRandomizer";
+import React from "react";
+
+function Phrase(props) {
+  const [reverse, setReverse] = React.useState(false);
+  const [show, setShow] = React.useState(false);
+
+  const wr = (w, r) => w.map((i) => (r ? i.translate : i.word)).join(" ");
+
+  const ac = getAdjCaseRandom(null, props.randCase.case);
+  const p = getPronRandom(props.randVerb.pron);
+
+  return (
+    <div style={{ display: "flex" }}>
+      <div
+        style={{
+          backgroundColor: show ? "white" : "black",
+          width: "fit-content",
+          padding: "10px 20px",
+          cursor: "pointer",
+        }}
+        onMouseEnter={(_) => setShow(true)}
+        onMouseLeave={(_) => setShow(false)}
+      >
+        {wr([p, props.randVerb, ac, props.randCase], reverse)}
+      </div>
+      <button onClick={(_) => setReverse(!reverse)}>r</button>
+      <div
+        style={{
+          width: "fit-content",
+          padding: "10px 20px",
+        }}
+      >
+        {wr([p, props.randVerb, ac, props.randCase], !reverse)}
+      </div>
+    </div>
+  );
+}
+
+export default Phrase;
--- frontend/src/components/pages/phrasePage/PhrasePage.js
+++ frontend/src/components/pages/phrasePage/PhrasePage.js
@@ -1,25 +1,22 @@
 import { useSelector } from "react-redux";
+import Phrase from "./Phrase";
 import React from "react";
 
 import {
   getVerbDeclensionRandom,
   getNounCaseRandom,
-  getAdjCaseRandom,
   getCaseRandom,
-  getPronRandom,
-} from "../../../reducers/wordRandomizer";
+} from "Reducers/wordRandomizer";
 
 function PhrasePage() {
   const verbDeclension = useSelector((state) => state.words.verbDeclension);
   const nounCase = useSelector((state) => state.words.nounCase);
 
-  const [randWord, setRandWord] = React.useState(null);
+  const [randVerb, setRandVerb] = React.useState(null);
   const [randCase, setRandCase] = React.useState(null);
-  const [reverse, setReverse] = React.useState(false);
-  const [show, setShow] = React.useState(false);
 
   const changeRandWord = (_) => {
-    setRandWord(getVerbDeclensionRandom());
+    setRandVerb(getVerbDeclensionRandom());
     setRandCase(getNounCaseRandom(null, getCaseRandom("accusative").id));
   };
 
@@ -32,42 +29,11 @@ function PhrasePage() {
     [verbDeclension, nounCase]
   );
 
-  const GetPhrase = (_) => {
-    const p = getPronRandom(randWord.pron);
-    const ac = getAdjCaseRandom(null, randCase.case);
-    const wr = (w, r) => w.map((i) => (r ? i.translate : i.word)).join(" ");
-    return (
-      <div style={{ display: "flex" }}>
-        <div
-          style={{
-            backgroundColor: show ? "white" : "black",
-            width: "fit-content",
-            padding: "10px 20px",
-            cursor: "pointer",
-          }}
-          onMouseEnter={(_) => setShow(true)}
-          onMouseLeave={(_) => setShow(false)}
-        >
-          {wr([p, randWord, ac, randCase], reverse)}
-        </div>
-        <button onClick={(_) => setReverse(!reverse)}>r</button>
-        <div
-          style={{
-            width: "fit-content",
-            padding: "10px 20px",
-          }}
-        >
-          {wr([p, randWord, ac, randCase], !reverse)}
-        </div>
-      </div>
-    );
-  };
-
-  if (randWord === null) return null;
+  if (randVerb === null) return null;
   return (
     <div>
       <div>
-        <GetPhrase />
+        <Phrase randVerb={randVerb} randCase={randCase} />
       </div>
       <button onClick={changeRandWord}>new</button>
     </div>
