04b0013d93a5e58bfc4d92db56daa8f9a9dfb6b3
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-11-19 20:30:05 +0100

    add phrase choice

--- frontend/src/components/pages/phrasePage/PhrasePage.js
+++ frontend/src/components/pages/phrasePage/PhrasePage.js
@@ -10,39 +10,58 @@ import {
 } from "./phraseConstructor";
 
 const cx = className.bind(style);
-const ph = PHRASE_NAME.ja;
 
 function PhrasePage() {
+  const [phrase, setPhrase] = React.useState(PHRASE_NAME.hslr);
   const [reverse, setReverse] = React.useState(false);
   const [words, setWords] = React.useState(null);
   const [show, setShow] = React.useState(false);
 
   React.useEffect(() => {
-    setWords(getAllWords(ph));
-  }, []);
+    setWords(getAllWords(phrase));
+  }, [phrase]);
 
   if (words === null) return null;
   return (
-    <div>
-      <div className={cx("empty")} />
-      <div className={cx("main-module")}>
-        <div
-          onClick={() => setWords(getAllWords(ph))}
-          onMouseLeave={() => setShow(false)}
-          onMouseEnter={() => setShow(true)}
-          className={cx("word")}
+    <div className={cx("phrase-main")}>
+      <div className={cx("phrase-side")}>
+        <select
+          className={cx("phrase-select")}
+          value={phrase}
+          onChange={(e) => setPhrase(e.target.value)}
         >
-          {show ? phraseConstructor(words, ph, reverse) : "xxxxxxxxxx"}
-        </div>
-        <img
-          src={reverseIcon}
-          onClick={() => setReverse(!reverse)}
-          className={cx("icon")}
-        />
-        <div className={cx("word")} onClick={() => setWords(getAllWords(ph))}>
-          {phraseConstructor(words, ph, !reverse)}
+          {Object.values(PHRASE_NAME).map((k) => (
+            <option value={k} key={k}>
+              {k}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className={cx("phrase-center")}>
+        <div className={cx("empty")} />
+        <div className={cx("phrase-card")}>
+          <div
+            onClick={() => setWords(getAllWords(phrase))}
+            onMouseLeave={() => setShow(false)}
+            onMouseEnter={() => setShow(true)}
+            className={cx("word")}
+          >
+            {show ? phraseConstructor(words, phrase, reverse) : "xxxxxxxxxx"}
+          </div>
+          <img
+            src={reverseIcon}
+            onClick={() => setReverse(!reverse)}
+            className={cx("icon")}
+          />
+          <div
+            className={cx("word")}
+            onClick={() => setWords(getAllWords(phrase))}
+          >
+            {phraseConstructor(words, phrase, !reverse)}
+          </div>
         </div>
       </div>
+      <div className={cx("phrase-side")} />
     </div>
   );
 }
