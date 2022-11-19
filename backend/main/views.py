0594f1c729e0c8419c8666da720b177785042bc2
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-11-19 11:05:33 +0100

    add bulk verPron create method

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,5 +1,7 @@
-from rest_framework.viewsets import ModelViewSet
 from .models import Pron, Verb, VerbDeclension, Noun, Case, NounCase, Adjective, AdjCase
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .serializer import (
     VerbDeclensionSerializer,
@@ -51,3 +53,20 @@ class VerbViewSet(ModelViewSet):
 class VerbDeclensionViewSet(ModelViewSet):
     serializer_class = VerbDeclensionSerializer
     queryset = VerbDeclension.objects.all()
+
+    @action(detail=False, methods=["POST"])
+    def bulk(self, request, *args, **kwargs):
+        v = VerbSerializer(data=request.data["verb"])
+        v.is_valid(raise_exception=True)
+        v.save()
+        print(v.data)
+        for x in request.data["verb_pron"]:
+            p = Pron.objects.get(id=x["pron"])
+            p = PronSerializer(instance=p)
+            vd = VerbDeclensionSerializer(
+                data={"verb": v.instance.id, **x, "pron": p.instance.id}
+            )
+            vd.is_valid(raise_exception=True)
+            vd.save()
+            print(vd.data)
+        return Response({"message": "ok"})
