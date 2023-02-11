3c65e2c4923e6048ce2c4170ede0f8146354679e
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2023-02-11 08:11:26 +0100

    add new constants

--- frontend/src/components/pages/phrasePage/phraseConstructor.js
+++ frontend/src/components/pages/phrasePage/phraseConstructor.js
@@ -2,7 +2,9 @@ import { getRandomWords } from "Reducers/wordRandomizer";
 
 export const PHRASE_NAME = {
   hslr: "я вижу красивую реку",
+  bt: "красивый стол",
   hs: "его сестра",
+  ys: "я вижу",
   ja: "я",
 };
 
@@ -10,7 +12,10 @@ export function getAllWords(phrase) {
   switch (phrase) {
     case PHRASE_NAME.hslr:
       return getRandomWords({ case: 2 });
+    case PHRASE_NAME.bt:
+      return getRandomWords({ case: 1 });
     case PHRASE_NAME.ja:
+    case PHRASE_NAME.ys:
     case PHRASE_NAME.hs:
     default:
       return getRandomWords();
@@ -26,6 +31,10 @@ export function phraseConstructor(words, phrase, reverse) {
       return ph([words.pron]);
     case PHRASE_NAME.hs:
       return ph([words.pronoun, words.noun]);
+    case PHRASE_NAME.ys:
+      return ph([words.pron, words.verbPron]);
+    case PHRASE_NAME.bt:
+      return ph([words.adjCase, words.nounCase]);
     case PHRASE_NAME.hslr:
     default:
       return ph([words.pron, words.verbPron, words.adjCase, words.nounCase]);
