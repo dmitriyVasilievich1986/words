2e560e87217cff31b761ee16333e29bb8d378a39
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2023-02-17 19:28:53 +0100

    remove old models

--- backend/main/models/pron_models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Pron(models.Model):
-    translate = models.CharField(max_length=150, blank=False, null=False)
-    word = models.CharField(max_length=150, blank=False, null=False)
