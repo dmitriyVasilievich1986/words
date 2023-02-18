b0895e137fe2feab859fbabc6a2d0c11f8de8b2c
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2023-02-18 10:51:00 +0100

    fix cache

--- backend/main/models/random_mixin.py
+++ backend/main/models/random_mixin.py
@@ -4,10 +4,11 @@ from random import choice
 
 class RandomMixin:
     @classmethod
-    def _random(cls, cache=[None], **kwargs):
+    def _random(cls, cache={}, **kwargs):
         objects = cls.objects.filter(**kwargs)
         if objects.count() <= 1:
+            cache[cls.__name__] = None
             return objects.first()
-        instance = choice(objects.filter(~Q(id=cache[0])))
-        cache[0] = instance.id
+        instance = choice(objects.filter(~Q(id=cache.get(cls.__name__))))
+        cache[cls.__name__] = instance.id
         return instance
