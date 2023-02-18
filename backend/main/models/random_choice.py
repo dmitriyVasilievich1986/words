61796f4899ac36060cbfe0c85f838fa45daf2da1
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2023-02-18 10:53:04 +0100

    add new rand choice

--- backend/main/models/random_choice.py
+++ backend/main/models/random_choice.py
@@ -1,4 +1,5 @@
 from .verb_models import VerbInfinitive, Verb
+from .pronoun_models import PersonalPronoun
 from dataclasses import dataclass
 from typing import Any
 
@@ -26,10 +27,26 @@ class Answer(dict):
             self["base"] = instance.base
 
 
+def personal_pron_verb(**kwargs):
+    pp = PersonalPronoun._random()
+    vi = VerbInfinitive._random()
+    v = vi.verb.get(pronoun=pp.id)
+    return [
+        Answer(pp, hiden=True),
+        Answer(),
+        Answer(v, hiden=True),
+    ]
+
+
 RANDOM_CHOICES = [
     RandomChoice(
         random=lambda **k: [Answer(hiden=True, instance=VerbInfinitive._random(**k))],
         description="Только глагол",
         name="Глагол",
+    ),
+    RandomChoice(
+        name="Личное местоимение + Глагол",
+        description="Только глагол",
+        random=personal_pron_verb,
     ),
 ]
