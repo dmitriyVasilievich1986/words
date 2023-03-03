918ec2cf15ee2163b78c849f2b44e164fb062443
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2023-03-03 23:20:01 +0100

    change Pronoun model

--- backend/main/models/pronoun_models.py
+++ backend/main/models/pronoun_models.py
@@ -1,3 +1,4 @@
+from .declentions_models import Declentions
 from .random_mixin import RandomMixin
 from .gender_models import Gender
 from django.db import models
@@ -14,6 +15,18 @@ class Pronoun(models.Model, RandomMixin):
     word = models.CharField(max_length=150, blank=False, null=False)
     plural = models.BooleanField(default=False)
 
+    personal_pronoun = models.ForeignKey(
+        on_delete=models.CASCADE,
+        related_name="pronoun",
+        to=PersonalPronoun,
+        null=True,
+    )
+    declentions = models.ForeignKey(
+        on_delete=models.CASCADE,
+        related_name="pronoun",
+        to=Declentions,
+        null=True,
+    )
     gender = models.ForeignKey(
         on_delete=models.CASCADE,
         related_name="pronoun",
