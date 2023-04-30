e924bb96d02b8156250e56e7d6cfc8222ace5dd1
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2023-04-30 19:04:30 +0200

    remove preposition from noun

--- backend/noun/models.py
+++ backend/noun/models.py
@@ -14,11 +14,6 @@ class Noun(RepresentationBaseClass, models.Model, RandomMixin):
         blank=True,
         to=Tags,
     )
-    prepositions = models.ManyToManyField(
-        related_name="noun",
-        to=Preposition,
-        blank=True,
-    )
     declention = models.ForeignKey(
         on_delete=models.CASCADE,
         related_name="noun",
@@ -35,8 +30,6 @@ class Noun(RepresentationBaseClass, models.Model, RandomMixin):
     def __repr__(self) -> str:
         plural = f"Plural: {self.plural}"
         gender = self.gender and f"Gender: <<{self.gender}>>"
+        tags = f"Tags: <<{[str(x) for x in self.tags.all()]}>>"
         declention = self.declention and f"Declention: <<{self.declention}>>"
-        preposition = (
-            self.prepositions and f"Prepositions: <<{self.prepositions.all()}>>"
-        )
-        return super().__repr__(plural, gender, declention, preposition)
+        return super().__repr__(plural, gender, declention, tags)
