9f29c4053bd77a7ff4021364489b3f4a65384b01
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2023-05-07 21:29:57 +0200

    remodel select component

--- frontend/src/components/pages/createWord/CreateWordPage.js
+++ frontend/src/components/pages/createWord/CreateWordPage.js
@@ -1,4 +1,4 @@
-import { TextInput, BoolInput, Select, Button } from "../mainComponents";
+import { Select, Button } from "../mainComponents";
 import className from "classnames";
 import style from "./style.scss";
 import React from "react";
@@ -8,57 +8,74 @@ const cx = className.bind(style);
 const initialWord = { id: 0, word: "new word", translate: "", base: "" };
 
 function CreateWordPage() {
-  const [wordList, setWordList] = React.useState([initialWord]);
-  const [newWord, setNewWord] = React.useState(initialWord);
-  const [models, setModels] = React.useState(null);
-  const [word, setWord] = React.useState(null);
-  const [data, setData] = React.useState([]);
+  const [selectedPartOfSpeech, setSelectedPartOfSpeech] = React.useState(null);
+  const [wordsList, setWordsList] = React.useState([initialWord]);
+  const [partsOfSpeech, setPartsOfSpeech] = React.useState(null);
+  const [selectedWord, setSelectedWord] = React.useState(null);
+  const [wordModel, setWordModel] = React.useState([]);
+
+  const [postData, dispatch] = React.useReducer(function (state, action) {
+    return { ...state, ...action };
+  }, {});
 
   React.useEffect(() => {
     axios
       .get("/api/model/")
       .then((data) => {
-        setWord(data.data[0].id);
-        setModels(data.data);
+        setPartsOfSpeech(data.data);
+        setSelectedPartOfSpeech(data.data[0]);
       })
       .catch((e) => console.log(e));
   }, []);
 
   React.useEffect(() => {
-    if (word === null) return;
-    setNewWord(initialWord);
+    if (selectedPartOfSpeech === null) return;
     axios
-      .get(`${models[word].url}model/`)
+      .get(`${selectedPartOfSpeech.url}model/`)
       .then((data) => {
-        setData(data.data);
+        dispatch(Object.fromEntries(data.data.map((v) => [v.name, v.data])));
+        setWordModel(data.data);
       })
       .catch((e) => console.log(e));
     axios
-      .get(`${models[word].url}`)
+      .get(`${selectedPartOfSpeech.url}`)
       .then((data) => {
-        setWordList([newWord, ...data.data]);
+        setWordsList(data.data);
       })
       .catch((e) => console.log(e));
-  }, [word]);
+  }, [selectedPartOfSpeech]);
 
-  const postData = (submitData) => {
+  const dispatchWord = (word) => {
+    dispatch(
+      Object.fromEntries(
+        Object.keys(word)
+          .filter((k) => k !== "id")
+          .map((k) => [k, word[k]])
+      )
+    );
+  };
+
+  const sendPostRequest = () => {
     axios
-      .post(models[word].url, submitData)
+      .post(selectedPartOfSpeech.url, postData)
       .then((data) => {
-        setWordList([...wordList, data.data]);
-        setNewWord(initialWord);
+        setWordsList([...wordsList, data.data]);
+        dispatchWord(data.data);
+        setSelectedWord(data.data);
       })
       .catch((e) => {
         console.log(e);
       });
   };
 
-  const patchData = (submitData) => {
+  const sendPatchRequest = () => {
     axios
-      .patch(`${models[word].url}${newWord.id}/`, submitData)
+      .patch(`${selectedPartOfSpeech.url}${selectedWord.id}/`, postData)
       .then((data) => {
-        setWordList(wordList.map((w) => (w.id == newWord.id ? data.data : w)));
-        setNewWord(initialWord);
+        setWordsList(
+          wordsList.map((w) => (w.id == selectedWord.id ? data.data : w))
+        );
+        dispatchWord(data.data);
       })
       .catch((e) => {
         console.log(e);
@@ -66,48 +83,118 @@ function CreateWordPage() {
   };
 
   const submitHandler = (e) => {
-    const getValue = (v) => {
-      if (v.includes(",")) return v.split(",").map((x) => Number(x));
-      else if (["false", "true"].includes(v)) return v == "true";
-      else if (v == -1) return null;
-      else if (!isNaN(v)) return Number(v) || [];
-      return v;
-    };
     e.preventDefault();
-    const submitData = {};
-    Array.from(e.target.getElementsByTagName("input")).map((i) => {
-      submitData[i.name] = getValue(i.value);
-    });
-    if (newWord.id == 0) postData(submitData);
-    else patchData(submitData);
+    if (selectedWord?.id) sendPatchRequest();
+    else sendPostRequest();
   };
 
-  const WordConstructor = (props) => {
-    switch (props.component) {
-      case "choiceInput":
-        return <Select {...props} default={newWord[props.name]} />;
-      case "boolInput":
-        return <BoolInput {...props} default={newWord[props.name]} />;
-      default:
-        return <TextInput {...props} default={newWord[props.name]} />;
-    }
-  };
-
-  if (models === null) return null;
+  if (!selectedPartOfSpeech) return null;
   return (
     <div className={cx("create-word-window")}>
       <div className={cx("wrapper")}>
-        <Select value={models} onChange={(e) => setWord(e[0].id)} />
+        <div
+          style={{ width: "100%", display: "flex", justifyContent: "center" }}
+        >
+          <Select
+            onChange={(p) => {
+              if (p !== null) {
+                setSelectedWord(null);
+                setSelectedPartOfSpeech(
+                  partsOfSpeech.find((ps) => ps.id === p)
+                );
+              }
+            }}
+            value={selectedPartOfSpeech.id}
+            options={partsOfSpeech}
+          />
+        </div>
         <form onSubmit={submitHandler}>
-          <div style={{ textAlign: "center" }}>"{newWord.word}"</div>
-          <div>
-            {data.map((d, i) => (
-              <WordConstructor key={i} {...d} />
-            ))}
+          <div style={{ textAlign: "center" }}>
+            "{selectedWord?.word || "New Word"}"
           </div>
-          <Button text={newWord.id == 0 ? "Send" : "Update"} />
+          <div>
+            {wordModel.map((d, i) => {
+              switch (d.component) {
+                case "textInput":
+                  return (
+                    <div
+                      className={cx("field-row")}
+                      key={`${selectedPartOfSpeech.word}_${d.name}`}
+                    >
+                      <span>{d.name}:</span>
+                      <input
+                        type="text"
+                        key={d.name}
+                        className={cx("input")}
+                        value={postData[d.name]}
+                        onChange={(e) => dispatch({ [d.name]: e.target.value })}
+                      />
+                    </div>
+                  );
+                case "boolInput":
+                  return (
+                    <div
+                      className={cx("field-row")}
+                      key={`${selectedPartOfSpeech.word}_${d.name}`}
+                    >
+                      <span>{d.name}:</span>
+                      <div
+                        style={{
+                          width: "70%",
+                          display: "flex",
+                          justifyContent: "center",
+                        }}
+                      >
+                        <input
+                          key={d.name}
+                          type="checkbox"
+                          value={postData[d.name]}
+                          onChange={(e) =>
+                            dispatch({ [d.name]: e.target.value })
+                          }
+                        />
+                      </div>
+                    </div>
+                  );
+                case "choiceInput":
+                  return (
+                    <div
+                      className={cx("field-row")}
+                      key={`${selectedPartOfSpeech.word}_${d.name}`}
+                    >
+                      <span>{d.name}:</span>
+                      <Select
+                        options={d.value}
+                        multiple={d?.multiple}
+                        value={postData[d.name]}
+                        onChange={(v) => dispatch({ [d.name]: v })}
+                      />
+                    </div>
+                  );
+                default:
+                  return null;
+              }
+            })}
+          </div>
+          <Button text={selectedWord?.id ? "Update" : "Send"} />
         </form>
-        <Select value={wordList} onChange={(v) => setNewWord(v[0])} />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Select
+            options={wordsList}
+            value={selectedWord?.id}
+            onChange={(v) => {
+              const newValue = v && wordsList.find((wl) => wl.id === v);
+              setSelectedWord(newValue);
+              if (v === null) {
+                dispatch(
+                  Object.fromEntries(wordModel.map((wm) => [wm.name, wm.data]))
+                );
+              } else {
+                dispatchWord(newValue);
+              }
+            }}
+          />
+        </div>
       </div>
     </div>
   );
