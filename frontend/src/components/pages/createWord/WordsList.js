3a2ea33d44630525317ca1649f782062f52026a6
dmitriy <dmitriyvasil@gmail.com>
2024-04-21 20:17:11 +0200

    add search filter

--- frontend/src/components/pages/createWord/WordsList.js
+++ frontend/src/components/pages/createWord/WordsList.js
@@ -7,23 +7,40 @@ const cx = classnames.bind(style);
 
 function WordsList({ infinitives }) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [search, setSearch] = React.useState("");
 
   const selectedInfinitive = searchParams.get("infinitive");
 
   return (
     <div className={cx("list")}>
+      <div className={cx("search")}>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
       <div className={cx("wrapper")}>
-        {infinitives.map((infinitive) => (
-          <div
-            key={infinitive.id}
-            onClick={() => setSearchParams({ infinitive: infinitive.id })}
-            className={cx({ isSelected: selectedInfinitive == infinitive.id })}
-          >
-            <div>
-              {infinitive.word} / {infinitive.translate}
+        {infinitives
+          .filter(
+            (i) =>
+              search === "" ||
+              i.word.toLowerCase().includes(search.toLowerCase()) ||
+              i.translate.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((infinitive) => (
+            <div
+              key={infinitive.id}
+              onClick={() => setSearchParams({ infinitive: infinitive.id })}
+              className={cx({
+                isSelected: selectedInfinitive == infinitive.id,
+              })}
+            >
+              <div>
+                {infinitive.word} / {infinitive.translate}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
